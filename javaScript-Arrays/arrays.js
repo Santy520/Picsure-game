f535eb1fdab8cca259c2d1fe0cf3c57ad1d11a00
Santy520 <sspalacio20@gmail.com>
2024-02-06 20:15:51 -0500

    array push

---
+++ javaScript-Arrays/arrays.js
@@ -0,0 +1,6 @@
+var images= ("new-york-image", "chicago-image", "vegas-image")
+
+var temperature= ("new-york-temp", "chicago-temp", "vegas-temp")
+
+var timezones= ("new-york-eastern", "chicago-central", "vegas-pacific")
+

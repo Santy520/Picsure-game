1b19b47f48ca81258e0e47585dad9c1751af5480
Patrick Sheehan <patrick@Patricks-MacBook-Air.local>
2024-02-06 20:49:30 -0500

    updated JS

--- script.js
+++ script.js
@@ -40,18 +40,33 @@ function playGame(array, i, score){
 
         /** Set up dummy data to display */
 
-        /** API CODE
-        let apiKey = '';
-        let perPage = '&per_page=5';
-        let baseURL = 'https://api.unsplash.com/photos/random/?';
-        let unsplashRequest = baseURL + 'query=' + currentLocation + '&' + apiKey + '&' + perPage;
+        let apiKeyUnsplash = '6ZuknqkaIj1xSrnF1lxmK1vVqUMZyAtTAOR4kLUmdac';
+        let queryUnsplash = 'https://api.unsplash.com/photos/random/?';
+        let unsplashRequest = queryUnsplash + 'query=' + currentLocation + '&' + apiKeyUnsplash;
 
-        fetch(unsplashRequest)
+        fetch(unsplashRequest).then(function (response) {
+        return response.json();
+        }).then(function (data) {
+            console.log(data);
+            let currentImage = response.data.urls.small;
+            console.log(currentImage);
+        })
+
+
+        //Get random photo from Unpslash using currentQuestion
+        //Get corresponding facts using currentQuestion
+        
+
+         /** API CODE FOR WEATHER
+        let apiKeyWeather = 'a34c2b8d2c8d04d52a0bced017c36070';
+        let weatherRequest = 'https://api.openweathermap.org/data/2.5/weather?q=${currentLocation}&appid=${apiKeyWeather}';
+    
+
+        fetch(weatherRequest)
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error))
 
-        //fetch random facts from API
         //Get random photo from Unpslash using currentQuestion
         //Get corresponding facts using currentQuestion
         */
@@ -87,5 +102,10 @@ function shuffleArray(array) {
         array[j] = temp;
     }
     return array;
+}
+
+function convertTemp(temp){
+    let farenheit = (temp - 273.15)*1.8 + 32;
+    return farenheit;
 }
 
